55f4fb229798591fa5b231bb23810cc5eb6bc520
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-08-18 02:55:26 -0400

    Add subsections, too

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,20 @@ $("aside").html(function(){
   let h1, sidebar;
   h1 = $("h1").html();
   $("h1").remove();
-  sidebar = "<h1>" + h1 + "</h1>\n<hr>\n<h3>Sections</h3>\n<nav class='nav flex-column'>";
+  sidebar = "<h1>" + h1 + "</h1>\n<hr>\n<h3>Sections</h3>\n<ul class='nav flex-column'>";
   $("section").each(function() {
+    sidebar = sidebar + "<li class='nav-item'>\n";
     sidebar = sidebar + "<a clas='nav-link' href='#" + $( this ).attr("id") + "'>" + $( this ).contents()[1].innerHTML + "</a>\n";
+    if ( $( this ).has("h3").length ){
+      sidebar = sidebar + "<ul>\n";
+      $( this ).find("h3").each(function(){
+        sidebar = sidebar + "<li class='nav-item'>\n";
+        sidebar = sidebar + "<a clas='nav-link' href='#" + $( this ).attr("id") + "'>" + $( this ).html() + "</a>\n";
+        sidebar = sidebar + "</li>\n";
+      });
+      sidebar = sidebar + "</ul>\n";
+    }
+    sidebar = sidebar + "</li>\n";
   });
-  return sidebar + "</nav>";
+  return sidebar + "</ul>\n";
 });
