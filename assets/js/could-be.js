8dba05a322ca2d3b88763e69bcaca369cf582d64
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-08-25 16:05:30 -0400

    Clean up examples, well, at least the final.

--- assets/js/could-be.js
+++ assets/js/could-be.js
@@ -1,113 +1,124 @@
+// Final version of the “Could Be” Project’s JavaScript.
+//
+// Note: Because of the specific way that the Project is uploaded to the
+// internet, no actual parsing of Markdown is done here. 
+//
+// Please follow along for this file by viewing the course at
+// http://the-javascripting-english-major.org/
+//
+
+// Leaflet section.
+// Define map and baselayer.
 let map, tileLayer;
-map = L.map("could-be-map");
+// Initialize map and set its center to Washington Sq. Park.
+map = L.map("could-be-map").setView([40.730833, -73.9975], 16);
+// Assign the baselayer and add it to the map.
 tileLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
               attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
               subdomains: "abcd",
               maxZoom: 18
             }).addTo(map);
-map.setView([40.730833, -73.9975], 16);
-let couldBeFeatures;
+// Acquire geographic GeoJSON data from an async request. The rest of 
+// the JavaScript will be callbacks in this function.
 $.getJSON("http://the-javascripting-english-major.org/could-be.geo.json", function(data){
-  let couldBeLayer;
+  // Define the markers’s data array and Leaflet layer of 
+  // circle markers.
+  let couldBeLayer, couldBeFeatures;
+  // Iterate over GeoJSON to build a new array of objects. 
+  // This avoids the use of L.geoJSON(), which hides the GeoJSON
+  // properties deep the layer itself.
   couldBeFeatures = data.features.map(function(feature){
     return {
       name: feature.properties.name,
+      html: feature.properties.html,
       div: feature.properties.div,
       mentions: feature.properties.mentions,
       lines: feature.properties.lines,
       wikipedia: feature.properties.wikipedia,
+      // Recall that GeoJSON presents coordinates as a 
+      // [lng, lat] array.
       latLng: L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
     };
   });
-  if (document.location.href.match(/[^\/]+$/)[0].match(/(be14|be.html)/) !== null){
-    couldBeLayer = L.featureGroup(couldBeFeatures.map(function(feature){
-      let popupContent, lines;
-      popupContent = "<h4>" + feature.name + "</h4>";
-      if (feature.lines.length > 1){
-        lines = "lines " + feature.lines.join(" and ");
-      } else {
-        lines = "line " + feature.lines[0];
-      }
-      popupContent = popupContent + "<p>" + feature.name + " is mentioned on " + lines + ".<br />";
-      popupContent = popupContent + "Read about " + feature.name + " on <a href='"+ feature.wikipedia + "'>Wikipedia</a>.</p>";
-      return L.circleMarker(feature.latLng, {
-        radius: 10 * Math.sqrt(feature.mentions),
-        color: "#d33682",
-        fillColor: "#d33682"
-        }).bindPopup(popupContent);
-      })
-    );
-  } else {
-    couldBeLayer = L.featureGroup(couldBeFeatures.map(function(feature){
-      return L.marker(feature.latLng);
-      })
-    );
-  }
-  couldBeLayer.addTo(map);
+  // Generate circle marker layer and assign rich popups.
+  couldBeLayer = L.featureGroup(couldBeFeatures.map(function(feature){
+    // Create popups.
+    let popupContent, lines;
+    popupContent = "<h4>" + feature.name + "</h4>";
+    if (feature.lines.length > 1){
+      lines = "lines " + feature.lines.join(" and ");
+    } else {
+      lines = "line " + feature.lines[0];
+    }
+    popupContent = popupContent + "<p>" + feature.name + " is mentioned on " + lines + ".<br />";
+    popupContent = popupContent + "Read about " + feature.name + " on <a href='"+ feature.wikipedia + "'>Wikipedia</a>.</p>";
+    // Create circle markers.
+    return L.circleMarker(feature.latLng, {
+      // Use square root because circle areas are proportional to r^2.
+      radius: 10 * Math.sqrt(feature.mentions),
+      color: "#d33682",
+      fillColor: "#d33682"
+      }).bindPopup(popupContent);
+    })
+  ).addTo(map);
+  // These two functions rely on couldBeFeatures, and they
+  // are defined below.
+  loadPoem(couldBeFeatures);
+  loadNavTabs(couldBeFeatures);
   map.fitBounds(couldBeLayer.getBounds());
   map.zoomOut(1);
 });
-if (document.location.href.match(/[^\/]+$/)[0].match(/be12/) !== null){
+
+// Functions that get called in the GeoJSON callback function
+
+let loadPoem, loadNavTabs;
+loadPoem = function(featuresArray){
   $.ajax({
-    url: "/markdown/hastings-street.html",
-    success: function(html){
-      $("#content").html(html);
-    }
-  });
-}
-if (document.location.href.match(/[^\/]+$/)[0].match(/be13/) !== null){
-  ["hastings-street", "eighteenth-and-vine",
-    "fifth-and-mound", "introduction",
-    "lenox-avenue", "rampart"].forEach(function(tab){
-      // Create a variable tab that has the name as a string.
-    $.ajax({
-      url: "/markdown/" + tab + ".html",
-      success: function(html){
-        $("#" + tab).html(html);
-      }
-    });
-  });
-}
-if (document.location.href.match(/[^\/]+$/)[0].match(/(be14|be.html)/) !== null){
-  let placesArray;
-  placesArray = [
-    {text: "Hastings Street", div: "hastings-street", html: "Hastings Street"},
-    {text: "Lenox Avenue", div: "lenox-avenue", html: "Lenox Avenue"},
-    {text: "18th & Vine", div: "eighteenth-and-vine", html: "18th &amp; Vine"},
-    {text: "5th & Mound", div: "fifth-and-mound", html: "5th &amp; Mound"},
-    {text: "Rampart", div: "rampart", html: "Rampart"}
-  ];
-  $.ajax({
+    // Note, this is not Markdown.
     url: "/markdown/poem.html",
     success: function(html){
+      // See Ch. 12 for how to do this with Markdown.
       $("#poem").html(html);
-      placesArray.forEach(function(place){
+      featuresArray.forEach(function(feature){
         $("#poem").html(function(_, oldHtml){
           let regex, newHtml;
-          regex = RegExp(place.html, "g");
-          newHtml = "<a href='#' data-tab='" + place.div + "'>" + place.html + "</a>";
+          regex = RegExp(feature.html, "g");
+          newHtml = "<a href='#' data-tab='" + feature.div + "'>" + feature.html + "</a>";
           return oldHtml.replace(regex, newHtml);
+        });
+        // While looping, make the navigation tabs also cause panning.
+        $("#nav-tabs a[href='#" + feature.div + "']").click(function(){
+          map.panTo(feature.latLng);
         });
       });
       $("#poem a").click(function(){
         let div, latLng;
         div = $( this ).data("tab");
-        $("#tabs-nav a[href='#" + div + "']").tab("show");
-        latLng = couldBeFeatures.filter(function(feature){
+        $("#nav-tabs a[href='#" + div + "']").tab("show");
+        latLng = featuresArray.filter(function(feature){
           return feature.div === div;
         })[0].latLng;
         map.panTo(latLng);
       });
     }
   });
-  ["hastings-street", "eighteenth-and-vine",
-    "fifth-and-mound", "introduction",
-    "lenox-avenue", "rampart"].forEach(function(tab){
-    $.ajax({
-      url: "/markdown/" + tab + ".html",
-      success: function(html){
-        $("#" + tab).html(html);
-      }
-    });
+};
+
+loadNavTabs = function(featuresArray){
+  // Create an array using the div properties of every object in
+  // couldBeFeatures, then add "introduction" to the end of that
+  // array.
+  featuresArray.map(function(feature){
+    return feature.div;
+  }).concat(["introduction"]).forEach(function(tab){
+    // Loop through the new array of div names and "introduction".
+      $.ajax({
+        // Note, this is not Markdown.
+        url: "/markdown/" + tab + ".html",
+        success: function(html){
+          // See Ch. 12 for how to do this in Markdown.
+          $("#" + tab).html(html);
+        }
+      });
   });
-}
+};
