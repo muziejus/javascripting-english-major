0d7300a193b800e899f21460264bd7cc90108a1a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-04-20 02:29:42 -0400

    fix typo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,12 @@ $("aside").append(function(){
     sidebar = sidebar + "<h3>Sections</h3>\n<ul class='nav flex-column'>";
     $("section").each(function() {
       sidebar = sidebar + "<li class='nav-item'>\n";
-      sidebar = sidebar + "<a clas='nav-link' href='#" + $( this ).attr("id") + "'>" + $( this ).find("h2").html() + "</a>\n";
+      sidebar = sidebar + "<a class='nav-link' href='#" + $( this ).attr("id") + "'>" + $( this ).find("h2").html() + "</a>\n";
       if ( $( this ).has("h3").length ){
         sidebar = sidebar + "<ul>\n";
         $( this ).find("h3").each(function(){
           sidebar = sidebar + "<li class='nav-item'>\n";
-          sidebar = sidebar + "<a clas='nav-link' href='#" + $( this ).attr("id") + "'>" + $( this ).html() + "</a>\n";
+          sidebar = sidebar + "<a class='nav-link' href='#" + $( this ).attr("id") + "'>" + $( this ).html() + "</a>\n";
           sidebar = sidebar + "</li>\n";
         });
         sidebar = sidebar + "</ul>\n";
