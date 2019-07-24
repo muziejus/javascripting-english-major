f3e844bab646f9a12a5c23cf1620e80efe61af57
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-24 15:35:36 -0400

    begin building out file structure in Gatsby.

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `The JavaScripting English Major`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    }
+  ]
+}
